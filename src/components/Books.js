929ba245a07a1d8f40bac85085b414a500d08c95
Usman Irfan <usmanirfan@Usmans-MacBook-Pro.local>
2020-11-11 02:27:58 -0500

    Added json file and displayed books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import LibraryBooks from '../data/LibraryBooks.json'
+import homepage from '../components/images/algorithms.jpg'
+import cosmos from '../components/images/cosmos.jpg'
 
 class Books extends Component {
+  
   render() {
+    var imageName = require('./images/algorithms.jpg');
     return (
       <div>
         Books
         <br />
-        {this.props.input_prop}
+        {LibraryBooks.map((props, src) => {
+            return  <img src={props.src} alt = {props.alt} width={200} height={200}/>
+        })}
       </div>
     );
   }
