383009ca4750f86ff810657227dc351a0720f39e
Usman Irfan <usmanirfan@Usmans-MBP.ht.home>
2020-11-11 21:14:14 -0500

    Added Add To Cart Button

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
 import LibraryBooks from '../data/LibraryBooks.json'
-import homepage from '../components/images/algorithms.jpg'
-import cosmos from '../components/images/cosmos.jpg'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import "./All.css";
 class Books extends Component {
   
+
+  abc() {
+    console.log("a");
+  }
   render() {
-    var imageName = require('./images/algorithms.jpg');
     return (
       <div>
         Books
         <br />
-        {LibraryBooks.map((props, src) => {
-            return  <img src={props.src} alt = {props.alt} width={200} height={200}/>
+        <Container>
+          <Row>
+        {LibraryBooks.map((props, index) => {
+            return  (
+              <Col xs={6} md={4}>
+                <div className="bookGrid columns">     
+                  <div className="images">
+                    <Image src={props.src} alt = {props.alt} rounded />
+                  </div>           
+                  <p>{props.name}</p>
+                  
+                  <div class="middle">
+                    <button class="reserveButton" onClick={this.abc}>Add To Cart</button>
+                </div>
+                  
+                </div>
+              </Col>
+            )
         })}
+         </Row>
+        </Container>
       </div>
     );
   }
